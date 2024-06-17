dae1c6f595a42569a24ea2a061de8f1702cf48b4
Aghakarim-dev <hemidzade1010@gmail.com>
2024-06-17 20:52:09 +0400

    test

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const currencyMenu = document.getElementById("currencyMenu")
 faChevronDown.addEventListener("click", () => {
     faChevronDown.classList.toggle("rotate-180");
     currencyMenu.classList.toggle("hidden")
+    overlay.classList.remove("bg-gray-200")
+
 
 })
 
@@ -16,5 +18,11 @@ const faShoppingCard = document.querySelector(".fa-cart-shopping")
 const overlay = document.getElementById("overlay")
 faShoppingCard.addEventListener("click", () =>{
     overlay.classList.toggle("bg-gray-200")
-    console.log(faShoppingCard);
 })
+
+document.addEventListener("click", (event) => {
+    if (!faChevronDown.contains(event.target) && !currencyMenu.contains(event.target)) {
+        faChevronDown.classList.remove("rotate-180");
+        currencyMenu.classList.add("hidden");
+    }
+}); 
