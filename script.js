381f32b921c5fa6c51b2fea2ad336a7d93b3fe1b
Aghakarim-dev <hemidzade1010@gmail.com>
2024-06-17 14:33:28 +0400

    some changing added

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const mobileMenu = document.querySelector(".mobile-menu")
 faBars.addEventListener("click", () => {
     mobileMenu.classList.toggle("hidden")
 })
+
+const faChevronDown = document.querySelector(".fa-chevron-down")
+const currencyMenu = document.getElementById("currencyMenu")
+faChevronDown.addEventListener("click", () => {
+    faChevronDown.classList.toggle("rotate-180");
+    currencyMenu.classList.toggle("hidden")
+
+})
+
+const faShoppingCard = document.querySelector(".fa-cart-shopping")
+const overlay = document.getElementById("overlay")
+faShoppingCard.addEventListener("click", () =>{
+    overlay.classList.toggle("bg-gray-200")
+    console.log(faShoppingCard);
+})
